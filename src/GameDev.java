7f3b25cf79d53727e27f1bb99c3e02117abcb61e
Shunsui2 <brayanvsqz011@gmail.com>
2026-09-07 10:05:01 -0300

    conociendo el array

--- src/GameDev.java
+++
@@ -1,9 +0,0 @@
-public class GameDev extends Personaje {
-    int inspiracion ;
-
-    @Override
-    void trabajar() {
-        //super.trabajar();
-        System.out.println("Creando Mecanicas de un videojuego");
-    }
-}
--- src/Programador.java
+++
@@ -1,5 +0,0 @@
-public class Programador extends Personaje {
-    int lenguaje_dominados ;
-
-
-}
